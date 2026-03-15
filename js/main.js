79771931066ee523bc3478a0fb5fe355dc770642
leoSwing1 <kevinleoswing@gmail.com>
2026-03-15 17:29:02 +0530

    added certificates

--- js/main.js
+++ js/main.js
@@ -333,3 +333,17 @@
     })();
 
 })(jQuery);
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide() {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+
+    index++;
+    if(index >= slides.length){
+        index = 0;
+    }
+}
+
+setInterval(showSlide, 3000);
